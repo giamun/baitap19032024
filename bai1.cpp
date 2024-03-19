1c2c247b460f9f54d0fe2476e00604d61eebcca7
giamun <158754967+giamun@users.noreply.github.com>
2024-03-19 14:58:48 +0700

    Add files via upload

---
+++ bai1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void boicua7()
+{
+	// Your code here!
+	for (int i = 10; i < 100; i++)
+	{
+		if (i % 7 == 0)
+		{
+			printf(" %d ", i);
+		}
+	}
+}
+
+int main()
+{
+	boicua7();
+	return 0;
+}
---
+++ bai2.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int kiemTraSoChinhPhuong(int n) {
+	int canBacHai = sqrt(n);
+	return (canBacHai * canBacHai == n);
+}
+
+int demSoChinhPhuong(int n) {
+	int soLuongSoChinhPhuong = 0;
+	for (int i = 1; i <= n; i++) {
+		if (kiemTraSoChinhPhuong(i)) {
+			soLuongSoChinhPhuong++;
+		}
+	}
+	return soLuongSoChinhPhuong;
+}
+
+int main() {
+	int n;
+
+	printf("Nhap n: ");
+	scanf("%d", &n);
+
+	int soLuongSoChinhPhuong = demSoChinhPhuong(n);
+	printf("So luong so chinh phuong nho hon %d la: %d\n", n, soLuongSoChinhPhuong);
+
+	printf("Danh sach cac so chinh phuong:\n");
+	for (int i = 1; i <= n; i++) {
+		if (kiemTraSoChinhPhuong(i)) {
+			printf("%d ", i);
+		}
+	}
+
+	return 0;
+}
